dc6ed1597e8d742e5797121b1d0e7d6ca71a6525
BillHHHuang <BillHuangg@gmail.com>
2015-02-01 21:27:40 +0800

    CSharp Class Template

---
+++ CSharp/Class/Class.cs
@@ -0,0 +1,37 @@
+﻿//
+//  $safeitemrootname$
+//  $rootnamespace$
+//
+//  Created by $username$ on $time$.
+//  Copyright (c) Bill. All rights reserved.
+//
+
+using System;
+using System.Collections.Generic;
+$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$using System.Text;
+$if$ ($targetframeworkversion$ >= 4.5)using System.Threading.Tasks;
+$endif$
+namespace $rootnamespace$
+{
+    class $safeitemrootname$
+    {
+
+    	#region [Field]
+        
+        #endregion
+
+        #region [Properties]
+        
+        #endregion
+
+        public $safeitemrootname$() {
+        	
+        }
+
+		#region [Utility]
+        
+        #endregion
+
+    }
+}
